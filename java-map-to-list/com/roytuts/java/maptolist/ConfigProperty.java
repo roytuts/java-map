8f8a240a9fe7072bd82aca079e78e111205784dc
Soumitra <projwebinfo@gmail.com>
2022-05-11 19:40:42 +0530

    Create ConfigProperty.java

---
+++ java-map-to-list/com/roytuts/java/maptolist/ConfigProperty.java
@@ -0,0 +1,21 @@
+package com.roytuts.java.maptolist;
+
+public class ConfigProperty {
+
+	private String name;
+	private String value;
+
+	public ConfigProperty(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
